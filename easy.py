4157da63793a043634b1425e5c2ef8f41e474846
Moritz Wilhelmy <moritz+git@wzff.de>
2016-07-17 18:16:23 +0200

    maybe this works a little bit

--- easy.py
+++ easy.py
@@ -2,34 +2,29 @@ from array import array
 import numpy as np
 import math
 import time
-import serial
+#import serial
 import pyaudio as pa
 
-p = pa.PyAudio()
+import gui
+
 CHUNK = 1024
 FORMAT = pa.paInt16
 CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 10
 
-stream = p.open(format=FORMAT,
+if __name__ == '__main__':
+    p = pa.PyAudio()
+    gui = GUI()
+    stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK)
-
-while True:
-    print("* recording")
-
-    frames = []
-
     for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = []
-        data = stream.read(CHUNK)
+        data = numpy.fromstring(stream.read(CHUNK), 'Int16')
         frames.append(data)
         print(data)
-
-    print("* done recording")
 
     stream.stop_stream()
     stream.close()
