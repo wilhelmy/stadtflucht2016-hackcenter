28f1bfcc008b2feee6352ec2bccf489a22d5ccab
Moritz Wilhelmy <moritz+git@wzff.de>
2016-07-17 18:00:07 +0200

    fft-test, already reads audio

---
+++ easy.py
@@ -0,0 +1,45 @@
+from array import array
+import numpy as np
+import math
+import time
+import serial
+import pyaudio as pa
+
+p = pa.PyAudio()
+CHUNK = 1024
+FORMAT = pa.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 10
+
+ser = serial.Serial(
+    port='/dev/ttyACM1',
+    baudrate=9600,
+    timeout=1,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+while True:
+    print("* recording")
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = []
+        data = stream.read(CHUNK)
+        frames.append(data)
+        print(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
